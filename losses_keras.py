f1fbef9ba306c2d7b3722743de362c7f00c64c39
ninpnin <vainoyrjanainen@icloud.com>
2022-06-12 09:54:52 +0300

    feat: support batch inputs; test all loss functions

--- losses_keras.py
+++ losses_keras.py
@@ -3,22 +3,27 @@ import tensorflow_probability as tfp
 tfd = tfp.distributions
 import numpy as np
 
+# Heteroscedastic Normal distribution loss
 def normal_heteroscedastic(y_true, y_pred):
-    mu_pred = y_pred[:,0]
-    sigma_pred = tf.math.exp(y_pred[:, 1])
-    loss = 0.0
-    no_of_examples = y_pred.shape[0]
-    for ix in range(no_of_examples):
-        dist = tfd.Normal(loc=mu_pred[ix], scale=sigma_pred[ix])
-        loss += dist.log_prob(y_true[ix])
-    return - loss / no_of_examples
+    if len(y_pred.shape) == 2:
+        mu_pred = y_pred[:,0]
+        sigma_pred = tf.math.exp(y_pred[:, 1])
+    elif len(y_pred.shape) == 1:
+        mu_pred = y_pred[0]
+        sigma_pred = tf.math.exp(y_pred[1])
 
+    dist = tfd.Normal(loc=mu_pred, scale=sigma_pred)
+    loss = dist.log_prob(y_true)
+    return - tf.reduce_mean(loss)
+
+# Poisson loss
 def poisson_loss(y_true, y_pred):
     mu_pred = tf.math.exp(y_pred)
     dist = tfd.Poisson(rate=mu_pred)
     loss = dist.log_prob(y_true)
     return - tf.reduce_mean(loss)
 
+#  Negative Binomial loss
 def negbin_loss(y_true, y_pred):
     # Batches
     if len(y_pred.shape) == 2:
@@ -43,13 +48,38 @@ def negbin_loss(y_true, y_pred):
         return - tf.reduce_mean(loss)
 
 def main():
+    # Test Normal heteroscedastic loss
+    y_pred = np.random.randn(2)
+    y_true = np.random.randn(1)
+    loss = normal_heteroscedastic(y_true, y_pred)
+    print(loss)
+
+    y_pred = np.random.randn(3,2)
+    y_true = np.random.randn(3)
+    loss = normal_heteroscedastic(y_true, y_pred)
+    print(loss)
+
+    # Test Poisson loss
+    y_pred = np.random.randn(1)
+    y_true = 2
+
+    loss = poisson_loss(y_true, y_pred)
+    print(loss)
+
+    y_pred = np.random.randn(3)
+    y_true = np.array([0,3,1])
+
+    loss = poisson_loss(y_true, y_pred)
+    print(loss)
+
+    # Test Negative Binomial loss
     y_pred = np.random.randn(2)
     y_true = 2
 
     loss = negbin_loss(y_true, y_pred)
     print(loss)
 
-    y_pred = np.random.randn(3,10)
+    y_pred = np.random.randn(3,2)
     y_true = np.array([0,3,1])
 
     loss = negbin_loss(y_true, y_pred)
